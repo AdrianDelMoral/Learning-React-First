58e4ee988eed82feb5f3abdba0aa188053976729
Adrián Del Moral <ifullstart@gmail.com>
2023-09-19 17:32:24 +0200

    Tic Tac Toe Componetized

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,37 +1,10 @@
 import { useState } from "react"
 import confetti from 'canvas-confetti'
 
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-// eslint-disable-next-line react/prop-types
-const Square = ({ children, isSelected, updateBoard, index }) => {
-
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+import { Square } from "./components/Square.jsx"
+import { TURNS } from "./constants.js"
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
+import { WinnerModal } from './components/WinnerModal.jsx'
 
 function App() {
   const [board, setBoard] = useState(
@@ -43,37 +16,12 @@ function App() {
   // null, es que no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null) 
 
-  const checkWinner = (boardToCheck) => {
-    // revisamos todas las combinaciones ganadoras
-    // para ver si X u O ganó
-    for(const combo of WINNER_COMBOS) {
-      const [a,b,c] = combo
-      if(
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-        ) {
-          return boardToCheck[a]
-        }
-    }
-    // si no hay ganador
-    return null
-  }
-
   // reiniciamos el array(tablero) el turno al por defecto que será la X, 
   // y el ganador a nulo que no hay ninguno aún
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
-  }
-
-  const checkEndGame = (newBoard) => {
-    // Revisamos si hay un empate
-    // si no hay más espacios vacós
-    // en el tablero
-
-    return newBoard.every((square) => square !== null)
   }
 
   const updateBoard = (index) => {
@@ -91,7 +39,7 @@ function App() {
     setTurn(newTurn)
     
     // Revisar si hay ganador
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinnerFrom(newBoard)
     if(newWinner) {
       setWinner(newWinner)
       confetti()
@@ -141,29 +89,7 @@ function App() {
         </Square>
       </section>
 
-      {
-        winner !== null && (
-          <section className="winner">
-            <div className="text">
-              <h2 className="">
-                {
-                winner === false
-                  ? 'Empate' 
-                  : 'Ganó:'
-                }
-              </h2>
-              
-              <header className="win">
-                {winner && <Square>{winner}</Square>}
-              </header>
-
-              <footer>
-                <button onClick={resetGame}>Empezad de nuevo</button>
-              </footer>
-            </div>
-          </section>
-        )
-      }
+      <WinnerModal resetGame={resetGame} winner={winner}/>
     </main>
   )
 }
